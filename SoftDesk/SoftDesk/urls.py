2cafbab1fea2ce6fd1b3915c08bed86252b183fe
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-03-03 10:45:07 +0100

    Added first view/serializers for projects

--- SoftDesk/SoftDesk/urls.py
+++ SoftDesk/SoftDesk/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
+from projects.views import ProjectViewset
+
+router = routers.SimpleRouter()
+router.register('project', ProjectViewset, basename='project')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
--- SoftDesk/projects/views.py
+++ SoftDesk/projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+ 
+from projects.models import Project, Contributor, Issue, Comment
+from projects.serializers import ProjectSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
+ 
+class ProjectViewset(ModelViewSet):
+ 
+    serializer_class = ProjectSerializer
+ 
+    def get_queryset(self):
+        return Project.objects.all()
