d6866e5e729715e27bbdeef477f8a73b7850c772
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-03-06 18:39:05 +0100

    Added serializer class mixin

--- SoftDesk/projects/views.py
+++ SoftDesk/projects/views.py
@@ -2,16 +2,12 @@ from rest_framework.viewsets import ModelViewSet
  
 from projects.models import Project, Contributor, Issue, Comment
 from projects.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorSerializer, IssueSerializer, CommentSerializer
+from projects.mixins import GetDetailSerializerClassMixin
  
-class ProjectViewset(ModelViewSet):
+class ProjectViewset(GetDetailSerializerClassMixin, ModelViewSet):
  
     serializer_class = ProjectListSerializer
     detail_serializer_class = ProjectDetailSerializer
  
     def get_queryset(self):
         return Project.objects.all()
-    
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class()
