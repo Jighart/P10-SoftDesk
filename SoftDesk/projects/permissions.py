a9c8181eed8578ce47d78bfd3e1b9be47c3c665e
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-03-11 21:52:57 +0100

    Fixed permissions

--- SoftDesk/projects/permissions.py
+++ SoftDesk/projects/permissions.py
@@ -1,45 +1,64 @@
 from rest_framework import permissions
-from rest_framework.generics import get_object_or_404
 
-from projects.models import Project, Issue, Comment
+from projects.models import Project, Contributor
 
+
+def check_contributor(user, project):
+    for contributor in Contributor.objects.filter(project=project.id):
+        if user == contributor.user:
+            return True
+    return False
 
 class ProjectPermissions(permissions.BasePermission):
+
+    message = 'You dont have permissions to do that.'
+
     def has_permission(self, request, view):
-        try:
-            project = get_object_or_404(Project, id=view.kwargs['project_pk'])
-            if request.method in permissions.SAFE_METHODS:
-                return project in Project.objects.filter(contributors__user=request.user)
-            return request.user == project.author
-        except KeyError:
-            return True
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ['retrieve', 'list']:
+            return check_contributor(request.user, obj)
+        elif view.action in ['update', 'partial_update', 'destroy']:
+            return request.user == obj.author
 
 
 class ContributorPermissions(permissions.BasePermission):
+
     def has_permission(self, request, view):
-        project = get_object_or_404(Project, id=view.kwargs['project_pk'])
-        if request.method in permissions.SAFE_METHODS:
-            return project in Project.objects.filter(contributors__user=request.user)
-        return request.user == project.author
+        if not request.user and request.user.is_authenticated:
+            return False
+
+        if view.action in ['retrieve', 'list']:
+            return check_contributor(request.user, Project.objects.filter(id=view.kwargs['projects_pk']).first())
+
+        elif view.action in ['update', 'partial_update', 'create', 'destroy']:
+            return request.user == Project.objects.filter(id=view.kwargs['projects_pk']).first().author
 
 
 class IssuePermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        project = get_object_or_404(Project, id=view.kwargs['project_pk'])
-        try:
-            issue = get_object_or_404(Issue, id=view.kwargs['issue_pk'])
-            return request.user == issue.author
-        except KeyError:
-            return project in Project.objects.filter(contributors__user=request.user)
+
+    message = 'You dont have permission to do that.'
+
+    def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+
+        if view.action in ['retrieve', 'list', 'create']:
+            return check_contributor(request.user, obj.project)
+        elif view.action in ['update', 'partial_update', 'destroy']:
+            return request.user == obj.author
 
 
 class CommentPermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        project = get_object_or_404(Project, id=view.kwargs['project_pk'])
-        try:
-            comment = get_object_or_404(Comment, id=view.kwargs['comment_pk'])
-            if request.method in permissions.SAFE_METHODS:
-                return project in Project.objects.filter(contributors__user=request.user)
-            return request.user == comment.author
-        except KeyError:
-            return project in Project.objects.filter(contributors__user=request.user)
+
+    message = 'You dont have permission to do that.'
+
+    def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+
+        if view.action in ['retrieve', 'list', 'create']:
+            return check_contributor(request.user, obj.issue.project)
+        elif view.action in ['update', 'partial_update', 'destroy']:
+            return request.user == obj.author
