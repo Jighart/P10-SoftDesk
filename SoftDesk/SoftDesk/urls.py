fa167e0497977ed90d6b298ec6e76ecc2deb661a
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-04-09 20:09:27 +0200

    Finalized testing, fixed permissions, redid DB

--- SoftDesk/SoftDesk/urls.py
+++ SoftDesk/SoftDesk/urls.py
@@ -1,18 +1,3 @@
-"""SoftDesk URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_nested import routers
@@ -21,17 +6,19 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from projects.views import ProjectViewset, IssueViewset, ContributorsViewset, CommentViewset
 from users.views import SignupViewset
 
+
 projects_router = routers.SimpleRouter()
 projects_router.register(r'projects/?', ProjectViewset, basename='projects')
 
-users_router = routers.NestedSimpleRouter(projects_router, r"projects/?", lookup="projects", trailing_slash=False)
-users_router.register(r"users/?", ContributorsViewset, basename="users", )
+users_router = routers.NestedSimpleRouter(projects_router, r'projects/?', lookup='projects', trailing_slash=False)
+users_router.register(r'users/?', ContributorsViewset, basename='users', )
 
-issues_router = routers.NestedSimpleRouter(projects_router, r"projects/?", lookup='projects', trailing_slash=False)
-issues_router.register(r"issues/?", IssueViewset, basename="issues")
+issues_router = routers.NestedSimpleRouter(projects_router, r'projects/?', lookup='projects', trailing_slash=False)
+issues_router.register(r'issues/?', IssueViewset, basename='issues')
 
-comments_router = routers.NestedSimpleRouter(issues_router, r"issues/?", lookup="issues", trailing_slash=False)
-comments_router.register(r"comments/?", CommentViewset, basename="comments", )
+comments_router = routers.NestedSimpleRouter(issues_router, r'issues/?', lookup='issues', trailing_slash=False)
+comments_router.register(r'comments/?', CommentViewset, basename='comments', )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
